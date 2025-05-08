02bc9d993c995681d4d1ea284f2d408a01416a59
Chris Dong <870565769@qq.com>
2025-05-09 01:04:22 +1000

    initial commit

---
+++ api/upload.js
@@ -0,0 +1,35 @@
+import { IncomingForm } from 'formidable';
+import fs from 'fs';
+import * as w3up from '@web3-storage/w3up-client';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).send('Method not allowed');
+
+  const form = new IncomingForm();
+  form.parse(req, async (err, fields, files) => {
+    if (err) return res.status(500).json({ error: 'Form parse error' });
+
+    const uploaded = files.file;
+    const buffer = fs.readFileSync(uploaded.filepath);
+    const blob = new Blob([buffer], { type: uploaded.mimetype });
+    const file = new File([blob], uploaded.originalFilename, { type: uploaded.mimetype });
+
+    try {
+      const client = await w3up.create();
+      await client.login(process.env.W3UP_KEY);
+      await client.setCurrentSpace(process.env.W3UP_SPACE);
+
+      const cid = await client.uploadFile(file);
+      res.json({ cid: cid.toString() });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ error: e.message });
+    }
+  });
+}
